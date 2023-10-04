2515a48451ccca8308657bba2de728d3632e9593
Anna Andarzhanova <a.andarjanova@yandex.ru>
2023-10-04 15:24:14 +0300

    Fix fixtures

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import random
 from faker import Faker
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -21,28 +20,12 @@ def driver():
 
 
 @pytest.fixture
-def login():
-    name_surname = faker.name().replace(' ', '')
-    num = str(random.randint(1000, 1999))
-    domain = faker.free_email().split('@')[1]
-    login = name_surname + num + '@' + domain
-    return login
-
-
-@pytest.fixture
-def password():
-    password = ''
-    while len(password) < 6:
-        password += str(random.randint(0, 9))
-    return password
-
-
-@pytest.fixture
-def registration(driver, login):
+def registration(driver):
+    email = faker.email()
     driver.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.LOGIN_HEADER))
     driver.find_element(*Locators.REGISTER_LINK).click()
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.REGISTRATION_HEADER))
     driver.find_element(*Locators.NAME).send_keys('Anna')
-    driver.find_element(*Locators.EMAIL).send_keys(login)
+    driver.find_element(*Locators.EMAIL).send_keys(email)
     return driver
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,12 +1,13 @@
+import random
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from locators import Locators
 
 
 class TestRegistration:
-    def test_success_registration_six_symbols_show_login_page(self, registration, password):
+    def test_success_registration_six_symbols_show_login_page(self, registration):
         driver = registration
-        driver.find_element(*Locators.PASSWORD).send_keys(password)
+        driver.find_element(*Locators.PASSWORD).send_keys(str(random.randint(100000, 999999)))
         driver.find_element(*Locators.REGISTER_BUTTON).click()
         text = WebDriverWait(driver, 3).until(
             expected_conditions.visibility_of_element_located(Locators.LOGIN_HEADER)).text
